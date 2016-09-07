c468806bf8e0ddad958ad2e0129a12a74a1b74cb
David Hellmann <davidhellmann.com@gmail.com>
2016-09-08 01:52:08 +0200

    Test Styles

--- ___src/assets/js/vue/_helloworld.vue
+++ ___src/assets/js/vue/_helloworld.vue
@@ -1,13 +1,28 @@
 <template>
-  <div>
+  <div class="hello">
     <h2>Hello World</h2>
     <div class="vue__inner">
       <p>
         {{ message }}
       </p>
+      <p>
+        <input v-model="message">
+      </p>
+      <pre>
+        {{ $data }}
+      </pre>
     </div>
   </div>
 </template>
+
+<style>
+  .hello {
+    background-color: #e8e8e8;
+    padding: 40px;
+  }
+</style>
+
+
 <script>
   export default{
     name: 'hello',
